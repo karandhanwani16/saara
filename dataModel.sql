7f1291cc79f80534319aa4a34543301fd1e853a0
karan dhanwani <karandhanwani20@gmail.com>
2024-06-20 02:15:21 +0530

    updated dataModel.sql to add products and brand

--- dataModel.sql
+++ dataModel.sql
@@ -15,11 +15,44 @@ CREATE TABLE 'users' (
 );
 
 -- Table Name: category
-	CREATE TABLE 'category' (
- 'category_id' bigint(20) NOT NULL,
- 'category_name' varchar(500) NOT NULL,
- 'category_created_by' bigint(20) NOT NULL,
- 'category_created_at' timestamp NOT NULL DEFAULT current_timestamp(),
- 'category_updated_by' bigint(20) NOT NULL,
- 'category_updated_at' timestamp NOT NULL DEFAULT current_timestamp()
-)
+CREATE TABLE 'category' (
+    'category_id' bigint(20) NOT NULL,
+    'category_name' varchar(500) NOT NULL,
+    'category_created_by' bigint(20) NOT NULL,
+    'category_created_at' timestamp NOT NULL DEFAULT current_timestamp(),
+    'category_updated_by' bigint(20) NOT NULL,
+    'category_updated_at' timestamp NOT NULL DEFAULT current_timestamp()
+);
+
+-- Table Name: products
+create table `products` (
+    `product_id` bigint(20) NOT NULL,
+    `category_id` bigint(20) NOT NULL,
+    `brand_id` bigint(20) NOT NULL,
+    `product_name` varchar(500) NOT NULL,
+    `product_barcode` varchar(500) NOT NULL,
+    `product_quantity` varchar(500) NOT NULL,
+    `product_code` varchar(500) NOT NULL,
+    `product_cost_price` varchar(500) NOT NULL,
+    `product_cgst_percentage` varchar(500) NOT NULL,
+    `product_sgst_percentage` varchar(500) NOT NULL,
+    `product_igst_percentage` varchar(500) NOT NULL,
+    `product_cost_post_gst` varchar(500) NOT NULL,
+    `product_selling_price` varchar(500) NOT NULL,
+    `product_parlour_price` varchar(500) NOT NULL,
+    `product_description` varchar(2000) NOT NULL,
+    `product_created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `product_created_by` bigint(20) NOT NULL,
+    `product_updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `product_updated_by` bigint(20) NOT NULL
+);
+
+-- Table Name: brands
+create table `brands` (
+    `brand_id` bigint(20) NOT NULL,
+    `brand_name` varchar(500) NOT NULL,
+    `brand_created_by` bigint(20) NOT NULL,
+    `brand_created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `brand_updated_by` bigint(20) NOT NULL,
+    `brand_updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+);
